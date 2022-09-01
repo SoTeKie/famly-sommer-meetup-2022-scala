ff2d1b579703d4f99fe45a2520848ecc31b39493
Kieran OReilly <mko@famly.co>
2022-09-01 10:53:59 +0200

    lol

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,4 +2,6 @@ object Main extends App {
   sealed trait Cell
   final case object DeadCell extends Cell
   final case object AliveCell extends Cell
+
+  def update(cells: List[List[Cell]]): List[List[Cell]] = ???
 }
