b8df084ab2e05ce1a81c484858e0cb114159e736
Kieran OReilly <mko@famly.co>
2022-09-01 11:00:18 +0200

    lol

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,5 +3,5 @@ object Main extends App {
   final case object DeadCell extends Cell
   final case object AliveCell extends Cell
 
-  def update(cells: List[List[Cell]]): List[List[Cell]] = cells
+  def update(cells: List[List[Cell]]): List[List[Cell]] = List(List(DeadCell, DeadCell, DeadCell), List(DeadCell, DeadCell, DeadCell), List(DeadCell, DeadCell, DeadCell))
 }
